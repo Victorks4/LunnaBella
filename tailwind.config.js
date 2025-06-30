f3025259a377e1bb976fbb0a02ccfda6033a64f9
NetoTTT <vitinsantosks4@gmail.com>
2025-06-30 09:52:18 -0300

    att

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
 	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
+		"./*.html",
 	],
 	prefix: "",
 	theme: {
@@ -115,4 +115,4 @@ export default {
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
